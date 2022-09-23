46338629415b3b9e9659856b09838d3d2165b8ee
Erich Garcia <ceo@qvapay.com>
2022-09-23 00:11:06 -0400

    Fix Selle Controller page

--- app/Http/Controllers/Seller/DashboardController.php
+++ app/Http/Controllers/Seller/DashboardController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers\Seller;
 
+use Carbon\Carbon;
 use App\Models\Order;
 use App\Models\Product;
-use Auth;
-use Carbon\Carbon;
-use DB;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
@@ -14,11 +14,11 @@ class DashboardController extends Controller
     {
         $data['products'] = filter_products(Product::where('user_id', Auth::user()->id)->orderBy('num_of_sale', 'desc'))->limit(12)->get();
         $data['last_7_days_sales'] = Order::where('created_at', '>=', Carbon::now()->subDays(7))
-                                ->where('seller_id', '=', Auth::user()->id)
-                                ->where('delivery_status', '=', 'delivered')
-                                ->select(DB::raw("sum(grand_total) as total, DATE_FORMAT(created_at, '%d %b') as date"))
-                                ->groupBy(DB::raw("DATE_FORMAT(created_at, '%Y-%m-%d')"))
-                                ->get()->pluck('total', 'date');  
+            ->where('seller_id', '=', Auth::user()->id)
+            ->where('delivery_status', '=', 'delivered')
+            ->select(DB::raw("sum(grand_total) as total, DATE_FORMAT(created_at, '%d %b') as date"))
+            ->groupBy(DB::raw("DATE_FORMAT(created_at, '%Y-%m-%d')"))
+            ->get()->pluck('total', 'date');
 
         return view('seller.dashboard', $data);
     }
--- app/Http/Controllers/SellerController.php
+++ app/Http/Controllers/SellerController.php
@@ -111,17 +111,6 @@ class SellerController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
