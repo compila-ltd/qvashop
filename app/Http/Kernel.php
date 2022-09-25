2b8a31d5662a1c5b883933b134a637ef8fb815f6
Erich Garcia <ceo@qvapay.com>
2022-09-24 23:53:49 -0400

    Fix JS

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -32,10 +32,11 @@ class Kernel extends HttpKernel
         \RenatoMarinho\LaravelPageSpeed\Middleware\ElideAttributes::class,
         \RenatoMarinho\LaravelPageSpeed\Middleware\InsertDNSPrefetch::class,
         \RenatoMarinho\LaravelPageSpeed\Middleware\RemoveComments::class,
+        //\RenatoMarinho\LaravelPageSpeed\Middleware\CollapseWhitespace::class, // Note: This middleware invokes "RemoveComments::class" before it runs.
+        //\RenatoMarinho\LaravelPageSpeed\Middleware\DeferJavascript::class,
+        
         //\RenatoMarinho\LaravelPageSpeed\Middleware\TrimUrls::class, 
         //\RenatoMarinho\LaravelPageSpeed\Middleware\RemoveQuotes::class,
-        \RenatoMarinho\LaravelPageSpeed\Middleware\CollapseWhitespace::class, // Note: This middleware invokes "RemoveComments::class" before it runs.
-        \RenatoMarinho\LaravelPageSpeed\Middleware\DeferJavascript::class,
     ];
 
     /**
