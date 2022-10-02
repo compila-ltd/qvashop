38a0af282da008ac28a66bdc165f7d8b99c37597
Erich Garcia <ceo@qvapay.com>
2022-10-02 10:59:02 -0400

    Blog fixes

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -9,15 +9,16 @@ use App\Models\Blog;
 
 class BlogController extends Controller
 {
-    public function __construct() {
-        // Staff Permission Check
+    // Staff Permission Check
+    public function __construct()
+    {
         $this->middleware(['permission:view_blogs'])->only('index');
         $this->middleware(['permission:add_blog'])->only('create');
         $this->middleware(['permission:edit_blog'])->only('edit');
         $this->middleware(['permission:delete_blog'])->only('destroy');
         $this->middleware(['permission:publish_blog'])->only('change_status');
     }
-    
+
     /**
      * Display a listing of the resource.
      *
@@ -27,15 +28,15 @@ class BlogController extends Controller
     {
         $sort_search = null;
         $blogs = Blog::orderBy('created_at', 'desc');
-        
-        if ($request->search != null){
-            $blogs = $blogs->where('title', 'like', '%'.$request->search.'%');
+
+        if ($request->search != null) {
+            $blogs = $blogs->where('title', 'like', '%' . $request->search . '%');
             $sort_search = $request->search;
         }
 
         $blogs = $blogs->paginate(15);
 
-        return view('backend.blog_system.blog.index', compact('blogs','sort_search'));
+        return view('backend.blog_system.blog.index', compact('blogs', 'sort_search'));
     }
 
     /**
@@ -57,26 +58,25 @@ class BlogController extends Controller
      */
     public function store(Request $request)
     {
-        
         $request->validate([
             'category_id' => 'required',
             'title' => 'required|max:255',
         ]);
 
         $blog = new Blog;
-        
+
         $blog->category_id = $request->category_id;
         $blog->title = $request->title;
         $blog->banner = $request->banner;
         $blog->slug = preg_replace('/[^A-Za-z0-9\-]/', '', str_replace(' ', '-', $request->slug));
         $blog->short_description = $request->short_description;
         $blog->description = $request->description;
-        
+
         $blog->meta_title = $request->meta_title;
         $blog->meta_img = $request->meta_img;
         $blog->meta_description = $request->meta_description;
         $blog->meta_keywords = $request->meta_keywords;
-        
+
         $blog->save();
 
         flash(translate('Blog post has been created successfully'))->success();
@@ -91,7 +91,6 @@ class BlogController extends Controller
      */
     public function show($id)
     {
-        
     }
 
     /**
@@ -104,8 +103,8 @@ class BlogController extends Controller
     {
         $blog = Blog::find($id);
         $blog_categories = BlogCategory::all();
-        
-        return view('backend.blog_system.blog.edit', compact('blog','blog_categories'));
+
+        return view('backend.blog_system.blog.edit', compact('blog', 'blog_categories'));
     }
 
     /**
@@ -116,7 +115,7 @@ class BlogController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
-    {        
+    {
         $request->validate([
             'category_id' => 'required',
             'title' => 'required|max:255',
@@ -130,22 +129,24 @@ class BlogController extends Controller
         $blog->slug = preg_replace('/[^A-Za-z0-9\-]/', '', str_replace(' ', '-', $request->slug));
         $blog->short_description = $request->short_description;
         $blog->description = $request->description;
-        
+
         $blog->meta_title = $request->meta_title;
         $blog->meta_img = $request->meta_img;
         $blog->meta_description = $request->meta_description;
         $blog->meta_keywords = $request->meta_keywords;
-        
+
         $blog->save();
 
         flash(translate('Blog post has been updated successfully'))->success();
         return redirect()->route('blog.index');
     }
-    
-    public function change_status(Request $request) {
+
+    // Change Status
+    public function change_status(Request $request)
+    {
         $blog = Blog::find($request->id);
         $blog->status = $request->status;
-        
+
         $blog->save();
         return 1;
     }
@@ -159,18 +160,25 @@ class BlogController extends Controller
     public function destroy($id)
     {
         Blog::find($id)->delete();
-        
+
         return redirect('admin/blogs');
     }
 
-
-    public function all_blog() {
+    // All Blogs
+    public function all_blog()
+    {
         $blogs = Blog::where('status', 1)->orderBy('created_at', 'desc')->paginate(12);
         return view("frontend.blog.listing", compact('blogs'));
     }
-    
-    public function blog_details($slug) {
-        $blog = Blog::where('slug', $slug)->first();
+
+    // Blog Details
+    public function blog_details($slug)
+    {
+        // Cache this Blog entry for 1 hour
+        $blog = cache()->remember('blog_' . $slug, 3600, function () use ($slug) {
+            return Blog::where('slug', $slug)->first();
+        });
+
         return view("frontend.blog.details", compact('blog'));
     }
 }
