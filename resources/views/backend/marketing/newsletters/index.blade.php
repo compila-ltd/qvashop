8df3160d61d0830bb9c61c1f402b95bebe63ab13
Erich Garcia <ceo@qvapay.com>
2022-10-04 09:11:26 -0400

    Fix CodeView on Marketing panel

--- resources/views/backend/marketing/newsletters/index.blade.php
+++ resources/views/backend/marketing/newsletters/index.blade.php
@@ -1,7 +1,6 @@
 @extends('backend.layouts.app')
 
 @section('content')
-
 <div class="row">
     <div class="col">
         <div class="card">
@@ -11,13 +10,13 @@
 
             <div class="card-body">
                 <form class="form-horizontal" action="{{ route('newsletters.send') }}" method="POST" enctype="multipart/form-data">
-                	@csrf
+                    @csrf
                     <div class="form-group row">
                         <label class="col-sm-2 col-from-label" for="name">{{ translate('Emails')}} ({{ translate('Users')}})</label>
                         <div class="col-sm-10">
                             <select class="form-control aiz-selectpicker" name="user_emails[]" multiple data-selected-text-format="count" data-actions-box="true">
                                 @foreach($users as $user)
-                                    <option value="{{$user->email}}">{{$user->email}}</option>
+                                <option value="{{$user->email}}">{{$user->email}}</option>
                                 @endforeach
                             </select>
                         </div>
@@ -27,7 +26,7 @@
                         <div class="col-sm-10">
                             <select class="form-control aiz-selectpicker" name="subscriber_emails[]" multiple data-selected-text-format="count" data-actions-box="true">
                                 @foreach($subscribers as $subscriber)
-                                    <option value="{{$subscriber->email}}">{{$subscriber->email}}</option>
+                                <option value="{{$subscriber->email}}">{{$subscriber->email}}</option>
                                 @endforeach
                             </select>
                         </div>
@@ -41,16 +40,15 @@
                     <div class="form-group row">
                         <label class="col-sm-2 col-from-label" for="name">{{ translate('Newsletter content')}}</label>
                         <div class="col-sm-10">
-                            <textarea rows="8" class="form-control aiz-text-editor" data-buttons='[["font", ["bold", "underline", "italic"]],["para", ["ul", "ol"]], ["insert", ["link", "picture"]],["view", ["undo","redo"]]]' name="content" required></textarea>
+                            <textarea rows="8" class="form-control aiz-text-editor" data-buttons='[["font", ["bold", "underline", "italic"]],["para", ["ul", "ol"]], ["insert", ["link", "picture"]],["view", ["undo","redo","codeview"]]]' name="content" required></textarea>
                         </div>
                     </div>
                     <div class="form-group mb-0 text-right">
                         <button type="submit" class="btn btn-primary">{{ translate('Send')}}</button>
                     </div>
-                  </form>
-              </div>
+                </form>
+            </div>
         </div>
     </div>
 </div>
-
 @endsection
