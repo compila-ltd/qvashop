6306300c26d155d682b9992ee9f7051ff577923b
Erich Garcia <ceo@qvapay.com>
2022-10-25 00:03:17 -0400

    Remove Facebook Chat

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -200,27 +200,6 @@
     </script>
     <!-- End Google tag -->
 
-    <!-- Facebook Chat Code -->
-    <script type="text/javascript">
-        window.fbAsyncInit = function() {
-            FB.init({
-                xfbml: true,
-                version: 'v3.3'
-            });
-        };
-        (function(d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) return;
-            js = d.createElement(s);
-            js.id = id;
-            js.src = "https://connect.facebook.net/{{ getFullLocale(App::getLocale()) }}/sdk/xfbml.customerchat.js";
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-    </script>
-    <div id="fb-root"></div>
-    <div class="fb-customerchat" attribution=setup_tool page_id="100291932862392"></div>
-    <!-- End Facebook Chat Code -->
-
     @include('frontend.flash')
     
     <script>
