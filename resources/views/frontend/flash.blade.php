76f5581e996adb7807752f0e0e3a1ecf2e2c8b1a
Erich Garcia <ceo@qvapay.com>
2022-10-24 23:33:13 -0400

    Remove Facebook Pixel

--- resources/views/frontend/flash.blade.php
+++ resources/views/frontend/flash.blade.php
@@ -3,19 +3,16 @@
     AIZ.plugins.notify('success', "{{ translate('Please login first') }}");
 </script>
 @endif
-
 @if (session('danger'))
 <script>
     AIZ.plugins.notify('danger', "{{ translate('Please login first') }}");
 </script>
 @endif
-
 @if (session('warning'))
 <script>
     AIZ.plugins.notify('warning', "{{ translate('Please login first') }}");
 </script>
 @endif
-
 @if (session('info'))
 <script>
     AIZ.plugins.notify('info', "{{ translate('Please login first') }}");
--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -200,33 +200,6 @@
     </script>
     <!-- End Google tag -->
 
-    <!-- Facebook Pixel Code -->
-    <script>
-        ! function(f, b, e, v, n, t, s) {
-            if (f.fbq) return;
-            n = f.fbq = function() {
-                n.callMethod ?
-                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-            };
-            if (!f._fbq) f._fbq = n;
-            n.push = n;
-            n.loaded = !0;
-            n.version = '2.0';
-            n.queue = [];
-            t = b.createElement(e);
-            t.async = !0;
-            t.src = v;
-            s = b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t, s)
-        }(window, document, 'script', "https://connect.facebook.net/{{ getFullLocale(App::getLocale()) }}/fbevents.js");
-        fbq('init', "1099529614262652");
-        fbq('track', 'PageView');
-    </script>
-    <noscript>
-        <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1099529614262652&ev=PageView&noscript=1" />
-    </noscript>
-    <!-- End Facebook Pixel Code -->
-
     <!-- Facebook Chat Code -->
     <script type="text/javascript">
         window.fbAsyncInit = function() {
