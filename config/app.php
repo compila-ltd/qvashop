b04b791e441bdd7652f56029cb148ae058ad0fdd
Erich Garcia <ceo@qvapay.com>
2022-11-05 19:20:41 -0400

    Timezone

--- config/app.php
+++ config/app.php
@@ -87,7 +87,7 @@ return [
     |
     */
 
-    'timezone' => env('APP_TIMEZONE', 'UTC'),
+    'timezone' => env('APP_TIMEZONE', 'America/Havana'),
 
     /*
     |--------------------------------------------------------------------------
