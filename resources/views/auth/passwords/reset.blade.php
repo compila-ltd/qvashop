4987af035b387d6cba6e2d2b9a2d80ca95f18519
WWZ90 <locuswin@gmail.com>
2023-09-19 09:37:28 -0400

    Adding the eye to see password and validating in Reset

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -8,7 +8,7 @@
                 <div class="bg-white rounded shadow-sm p-4 text-left">
                     <h1 class="h3 fw-600">{{ translate('Reset Password') }}</h1>
                     <p class="mb-4 opacity-60">{{ translate('Enter your email address and new password and confirm password.')}} </p>
-                    <form method="POST" action="{{ route('password.update.new') }}">
+                    <form method="POST" action="{{ route('password.update.new') }}" class="form-password">
                         @csrf
 
                         <div class="form-group">
@@ -32,8 +32,7 @@
                         </div>
 
                         <div class="form-group">
-                            <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" placeholder="{{ translate('New Password') }}" required>
-
+                            <input type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" placeholder="{{ translate('New Password') }}"  id="password" onkeyup='check();' required>
                             @if ($errors->has('password'))
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $errors->first('password') }}</strong>
@@ -41,12 +40,13 @@
                             @endif
                         </div>
 
-                        <div class="form-group">
-                            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" placeholder="{{ translate('Confirm Password') }}" required>
+                        <div class="form-group password-icon">
+                            <input type="password" class="form-control" placeholder="{{ translate('Confirm Password') }}" name="password_confirmation" id="password_confirm" onkeyup='check();' required>
+                            <i class="bi bi-eye-slash" id="togglePassword"></i>
                         </div>
 
                         <div class="form-group text-right">
-                            <button type="submit" class="btn btn-primary btn-block">
+                            <button type="submit" class="btn btn-primary btn-block" id="btn_submit">
                                 {{ translate('Reset Password') }}
                             </button>
                         </div>
@@ -56,4 +56,35 @@
         </div>
     </div>
 </div>
+@endsection
+
+@section('script')
+    <script type="text/javascript">
+
+        window.addEventListener("DOMContentLoaded", function () {
+            const togglePassword = document.querySelector("#togglePassword");
+
+            togglePassword.addEventListener("click", function (e) {
+                // toggle the type attribute
+                const type = password.getAttribute("type") === "password" ? "text" : "password";
+                const type_c = password_confirm.getAttribute("type") === "password" ? "text" : "password";
+                password.setAttribute("type", type);
+                password_confirm.setAttribute("type", type_c);
+                // toggle the eye / eye slash icon
+                this.classList.toggle("bi-eye");
+            });
+        });
+
+        var check = function() {
+            if (document.getElementById('password').value == document.getElementById('password_confirm').value) {
+                document.getElementById("password").classList.remove('password_error');
+                document.getElementById("password_confirm").classList.remove('password_error');
+                document.getElementById("btn_submit").disabled = false;
+            } else {
+                document.getElementById("password").classList.add('password_error');
+                document.getElementById("password_confirm").classList.add('password_error');
+                document.getElementById("btn_submit").disabled = true;
+            }
+        }
+    </script>
 @endsection
